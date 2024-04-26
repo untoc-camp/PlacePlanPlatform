471276c398755cd49fa423b6682cf068a0ac5881
RDavid32 <114576793+RDavid32@users.noreply.github.com>
2024-04-27 00:03:44 +0900

    유저 구현 및 헤더 생성 #21
    유저 구현 및 헤더 생성 #21

--- component/header.py
+++ component/header.py
@@ -2,13 +2,40 @@ import justpy as jp
 
 from component.color import MainColors
 from component.font import Font
+from model.user_model import UserModel, setUserModel
 
-# class Hedaer:
-#     def __init__ (self, route):
-#         self.route = route
-#         self.user = 
+class Header:
+    def __init__ (self, route):
+        self.route = route
     
-#     def _login_header(self, wp):
-    
-#     def show_header(self, wp):
-        
+    def _nonMemberHeaderShow(self, wp):
+        div = jp.Div(classes='flex h-16 bg-white shadow justify-between')
+        header_left = jp.Div(classes='flex-1 flex items-center')
+        text1_div = jp.Div(text="Text 1", classes="text-lg")
+        text2_div = jp.Div(text="Text 2", classes="text-lg ml-4")
+        text3_div = jp.Div(text="Text 3", classes="text-lg ml-4")
+
+        header_left.add(text1_div, text2_div, text3_div)
+
+        header_right = jp.Div(classes='flex-1 flex items-center justify-end')
+        text1_div = jp.Div(text="Text 1", classes="text-lg")
+        text2_div = jp.Div(text="Text 2", classes="text-lg")
+        text3_div = jp.Div(text="Text 3", classes="text-lg")
+
+        header_right.add(text1_div, text2_div, text3_div)
+
+        div.add(header_left, header_right)
+
+        wp.add(div)
+    def _userMember(self, wp):
+        pass
+
+    def show_header(self, wp):
+        self.user = UserModel(
+            {"user_id": "", "user_name": "test", "user_password": "test"}
+        )
+        if self.user.user_id == "":
+            self._nonMemberHeaderShow(wp)
+            
+        else:
+            self._userMember(wp)
--- provider/user_api.py
+++ provider/user_api.py
@@ -1,4 +1,5 @@
 import justpy as jp
+from provider.api import api
 async def Login(UserModel): 
-    return await jp.get(f'login/{UserModel.id}/{UserModel.password}/{UserModel.name}')
+    return await jp.get(f'{api.api}login/{UserModel.id}/{UserModel.password}/{UserModel.name}')
 
