5c79322b71efc20d4347ca8f223d1bd9a91393c6
Cheonseo <angel5337@naver.com>
2024-05-08 11:55:16 +0900

    메인, 소개 페이지 1차 완성
    구현 가능한 부분까지 완성

--- view/introduce_view.py
+++ view/introduce_view.py
@@ -10,29 +10,63 @@ def IntroduceView():
     
     main_colors = MainColors()
     font = Font()
+    # Introduction Section
+    
+    intro_div = jp.Div(classes='text-center',children=[
+        jp.P(text='　', classes='my-4'),
+        jp.P(text='일정 관리 플랫폼', style=f'color: {main_colors.MainColor};', classes=f'{font.Heading2_Bold}'),
+        jp.P(text='　', classes='my-10'),
+        jp.P(text='여러 명의 사람들과 약속을 맞추는데 어려움을 겪으신 적이 없으신가요?', classes=f'{font.Heading4_Regular} leading-loose', style=f'color: {main_colors.TextColor};'),
+        jp.P(text='수많은 회의들을 관리하는데 불편하신 적이 없으신가요?', classes=f'{font.Heading4_Regular} leading-loose', style=f'color: {main_colors.TextColor};'),
+        jp.P(text='시간 뿐만 아니라 장소도 같이 정하기 귀찮지 않으셨나요?', classes=f'{font.Heading4_Regular} leading-loose', style=f'color: {main_colors.TextColor};'),
+        jp.Div(classes='text-center leading-loose', children=[
+            jp.Span(text='여러 명의 사람들과 같이 협업하는 ', classes=f'{font.Heading4_Regular} ', style=f'color: {main_colors.TextColor};'),
+            jp.Span(text='PPP', style=f'color: {main_colors.MainColor};', classes=f'{font.Heading4_Bold}'), 
+            jp.Span(text='가 함께하겠습니다', classes=f'{font.Heading4_Regular}', style=f'color: {main_colors.TextColor};')
+        ]),
+        jp.P(text='　', classes='my-20')
+    ])
+    
+    # Action Button
+    action_div = jp.Div(style=f'color: {main_colors.MainColor};', classes='text-center', children=[
+        jp.A(text="일정 계획하기", href="/signin", classes=f'{font.Heading2_Bold} text-center', style=f"color: #000000")  # Added text-center class for center alignment
+    ])
 
-    wp.add(jp.P(text='.', classes='my-4'))
-    wp.add(jp.P(text='일정 관리 플랫폼', classes=f'{font.Heading2_Bold} text-center'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-10'))
-    wp.add(jp.P(text='여러 명의 사람들과 약속을 맞추는데 어려움을 겪으신 적이 없으신가요?', classes=f'{font.Heading4_Regular} text-center leading-loose'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='수많은 회의들을 관리하는데 불편하신 적이 없으신가요?', classes=f'{font.Heading4_Regular} text-center leading-loose'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='시간 뿐만 아니라 장소도 같이 정하기 귀찮지 않으셨나요?', classes=f'{font.Heading4_Regular} text-center leading-loose'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='여러명의 사람들과 같이 협업하는 플랫폼 PPP가 함께하겠습니다', classes=f'{font.Heading4_Regular} text-center leading-loose'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-20'))
-    wp.add(jp.A(text="일정 계획하기", href="/signin", classes=f'{font.Heading2_Bold} text-center', style=f"color: {main_colors.MainColor};"))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-4'))
-    wp.add(jp.P(text='약속과 장소를 한 번에', classes=f'{font.Heading2_Bold} text-left leading-normal'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='여러명과 여러개의', classes=f'{font.Heading3_Bold} text-left'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='약속을 한번에!', classes=f'{font.Heading3_Bold} text-left'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-20'))
-    wp.add(jp.P(text='약속을 편하게', classes=f'{font.Heading3_Bold} text-right'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='잡아보세요', classes=f'{font.Heading3_Bold} text-right'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-20'))
-    wp.add(jp.P(text='사용 방법', classes=f'{font.Heading3_Bold} text-center'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-5'))
-    wp.add(jp.P(text='간편하게 장소 및 일정 약속 만들기', classes=f'{font.Heading2_Bold} text-center'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-5'))
-    wp.add(jp.P(text='일정과 장소를 여러명의 사람들과 한번에 정해보세요.', classes=f'{font.Body1_Regular} text-center'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='다양한 약속들을 잡을 수 있습니다.', classes=f'{font.Body1_Regular} text-center'))  # Added text-center class for center alignment
-    wp.add(jp.P(text='.', classes='my-20'))
+    # Feature Section
+    feature_div = jp.Div(classes='text-left', style=f'margin-left: 40px; margin-right: 40px;', children=[
+        jp.P(text='　', classes='my-4'),
+        jp.P(text='약속과 장소를 한 번에', style=f'color: {main_colors.MainColor};', classes=f'{font.Heading2_Bold} leading-normal'),
+        jp.P(text='여러명과 여러개의', classes=f'{font.Heading3_Bold}'),
+        jp.P(text='약속을 한번에!', classes=f'{font.Heading3_Bold}'),
+        jp.P(text='　', classes='my-20'),
+        jp.P(text='약속을 편하게', classes=f'{font.Heading3_Bold} text-right'),
+        jp.P(text='잡아보세요', classes=f'{font.Heading3_Bold} text-right'),
+        jp.P(text='　', classes='my-20')
+    ])
+    
+    # Usage Section
+    usage_div = jp.Div(classes='text-center leading-loose', style=f'background-color: {main_colors.GreyColor};', children=[
+        jp.P(text='　', classes='my-10'),
+        jp.P(text='사용 방법', classes=f'{font.Heading3_Bold} text-center', style=f'color: {main_colors.TextColor};'),
+        jp.P(text='　', classes='my-5'),
+        jp.Div(classes='text-center', children=[
+            jp.Span(text='간편하게 장소 및 일정 ', classes=f'{font.Heading2_Bold}'),
+            jp.Span(text='약속 ', style=f'color: {main_colors.MainColor};', classes=f'{font.Heading2_Bold}'),
+            jp.Span(text='만들기', classes=f'{font.Heading2_Bold}')
+        ]),
+        jp.P(text='　', classes='my-5'),
+        jp.P(text='일정과 장소를 여러명의 사람들과 한번에 정해보세요.', classes=f'{font.Body1_Regular}', style=f'color: {main_colors.TextColor};'),  
+        jp.P(text='다양한 약속들을 잡을 수 있습니다.', classes=f'{font.Body1_Regular}', style=f'color: {main_colors.TextColor};'),  
+        jp.P(text='　', classes='my-20'),
+        jp.P(text='　', classes='my-20'),
+        jp.P(text='Copyright 2021. PPP & UntoC cl.All right reserved.', classes=f'{font.Small3}'),
+        jp.P(text='　', classes='my-20')  
+    ])
+    
+    # Adding sections to the webpage
+    wp.add(intro_div)
+    wp.add(action_div)
+    wp.add(feature_div)
+    wp.add(usage_div)
+    
     return wp
--- view/mainpage_view.py
+++ view/mainpage_view.py
@@ -11,8 +11,10 @@ def MainPageView():
     wp.add(jp.P(text='메인페이지', classes='text-5xl m-2'))
 
     link = jp.A(text='로그인 페이지 이동', href='/signin', classes='text-lg hover:underline',  ) #페이지 이동 얘시입니다 이거 보시고 하시면 됩니다.
+    link2 = jp.A(text='소개 페이지 이동', href='/introduce', classes='text-lg hover:underline',  )
     #link = jp.A(text='회원가입 페이지 이동', href='/signup', classes='text-lg hover:underline',  style=color.MainColor) #페이지 이동 얘시입니다 이거 보시고 하시면 됩니다.
     CalendarComponent().showCalendar(wp)
     wp.add(link)
+    wp.add(link2)
 
     return wp
