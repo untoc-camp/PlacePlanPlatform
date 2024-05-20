252bbe63a1355f3acad4a0f3f39c8026f24f20dc
eyhhhh <silver807@pusan.ac.kr>
2024-05-20 20:46:57 +0900

    비밀번호 찾기 #1
    헤더 추가

--- view/signin/find_password_view.py
+++ view/signin/find_password_view.py
@@ -1,6 +1,11 @@
 import justpy as jp
 
+from element.header import Header
+
 def FindPasswordView():
     wp = jp.WebPage()
+    header = Header("/signin")
+    header.show_header(wp)
+    
     wp.add(jp.P(text='비밀번호 찾기', classes='text-5xl m-2'))
     return wp
