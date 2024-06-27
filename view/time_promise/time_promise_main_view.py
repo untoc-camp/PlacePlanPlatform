aa16cf0dbe51c125943ef95c9960ac483ca2b69e
Kim Hansol <han1203sol@naver.com>
2024-06-28 02:59:37 +0900

    시간약속페이지 디자인 수정 #1
    시간 약속 페이지 디자인 수정
    약속 만들기 페이지 : 12,24시간 기준 선택 드롭다운 삭제.

--- view/time_promise/time_promise_main_view.py
+++ view/time_promise/time_promise_main_view.py
@@ -2,11 +2,21 @@ import justpy as jp
 
 from element.color import MainColors
 from element.font import Font
+from element.header import Header
 
 def TimePromiseMainView():
     wp = jp.WebPage()
-    wp.add(jp.P(text='시간약속페이지', classes='text-5xl m-2'))
-    AppointmentApp().add_appointment_page(wp=wp)
+    head = Header("/timepromise/make")
+    head.show_header(wp)
+    
+    # 상단 섹션
+    top_section = jp.Div(classes="flex justify-between items-center p-4 border-b", a=wp)
+    jp.P(text='가능한 시간', classes='text-xl font-semibold', a=top_section)
+    jp.Button(text='약속 만들기', classes='bg-blue-100 hover:bg-blue-200 text-blue-800 py-2 px-4 rounded-full flex items-center', a=top_section, style="margin-right: 10px;")
+    
+    appointment_app = AppointmentApp()
+    appointment_app.add_appointment_page(wp=wp)
+    
     return wp
 
 class AppointmentApp:
@@ -14,31 +24,38 @@ class AppointmentApp:
         self.appointments = []  # 약속 목록 저장 리스트
         self.font = Font()
         self.color = MainColors()
+        self.grid = None
+        self.add_button_div = None
 
     def add_appointment_page(self, wp):
-        # 약속 추가 버튼
-        add_button = jp.Button(text='약속 만들기', classes='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded m-2' + self.font.Body1_Bold, a=wp)
-        
         # 약속 목록을 담을 그리드
-        self.grid = jp.Div(classes="grid grid-cols-3 gap-4 m-4", a=wp)
+        self.grid = jp.Div(classes="grid grid-cols-2 md:grid-cols-3 gap-4 p-4", style="display: grid; grid-auto-flow: dense;", a=wp)
 
-        # 버튼 클릭하면 약속 추가
-        add_button.on('click', self.add_new_appointment)
+        # 추가하기 버튼
+        self.add_button_div = jp.Div(classes="border rounded-lg shadow-md p-4 flex justify-center items-center cursor-pointer hover:bg-gray-100", style="height: 180px; width: 100%; order: 9999;", a=self.grid)
+        jp.Icon(icon='add', classes='text-3xl text-gray-500', a=self.add_button_div)
+        jp.P(text='추가하기', classes='text-sm text-gray-500 mt-2', a=self.add_button_div)
+        
+        # 추가하기 버튼 클릭 이벤트
+        self.add_button_div.on('click', self.add_new_appointment_click)
 
         return wp
 
-    def add_new_appointment(self, msg):
+    def add_new_appointment_click(self, msg):
+        self.add_new_appointment()
+
+    def add_new_appointment(self):
         # 약속 담길 div 생성
-        appointment_div = jp.Div(classes = f"border-2 p-2 rounded", style = f"border-color: {self.color.MainColor};")
-        appointment_div.style = f"margin: 10px; border-color: {self.color.MainColor};"
+        appointment_div = jp.Div(classes="border rounded-lg shadow-md p-4", style="border-color: #e5e7eb; background-color: #ffffff; height: 180px;")
         self.appointments.append(appointment_div)  # 약속 목록에 추가
 
         # 약속 정보 추가
-        jp.P(text='약속 명', classes=self.font.Heading4_Bold, a=appointment_div)   # 추후에 변수 연결 예정 ------------------------- 
-        jp.Div(text='날짜: #날짜', classes='flex items-center', a=appointment_div)
-        jp.Div(text='참여 인원: #n명', classes='flex items-center', a=appointment_div)
-        jp.Div(text='위치: #위치명', classes='flex items-center', a=appointment_div)
-        jp.Div(text='공유하기', classes='flex items-center justify-end', a=appointment_div)
+        jp.P(text='약속명이 들어갈 공간입니다', classes="text-base font-medium text-gray-900 mb-2", a=appointment_div)  # 약속명
+        jp.Div(text='24.01.01 ~ 24.01.13', classes='text-sm text-gray-500 mb-1', a=appointment_div)  # 날짜
+        jp.Div(text='4명', classes='text-sm text-gray-500 mb-1', a=appointment_div)  # 참여 인원
+        jp.Div(text='미정', classes='text-sm text-gray-500 mb-2', a=appointment_div)  # 위치
+        jp.Button(text='공유하기', classes='bg-blue-100 hover:bg-blue-200 text-blue-800 text-sm py-1 px-3 rounded-full', a=appointment_div, style="margin-top: 10px;")
 
-        # 그리드에 약속 div 추가
-        appointment_div.add_to(self.grid)
+        # 그리드에 약속 추가
+        self.grid.add(appointment_div)
+
--- view/time_promise/time_promise_make_view.py
+++ view/time_promise/time_promise_make_view.py
@@ -9,19 +9,18 @@ class AppointmentPage:
         self.color = MainColors()
         self.font = Font()
         self.calendar = CalendarComponent()
-        self.custom_type_input = None  # 직접 작성 입력란을 저장할 변수
+        self.custom_place_input = None  # 직접 작성 입력란을 저장할 변수 (장소)
+        self.custom_type_input = None  # 직접 작성 입력란을 저장할 변수 (약속 종류)
+        self.appointment_place_select = None  # 약속 장소 선택란을 저장할 변수
         self.appointment_type_select = None  # 약속 종류 선택란을 저장할 변수
         self.calendar_div = jp.Div(classes='w-1/3 bg-white-100 p-4 rounded-md')
 
     def toggle_color(self, msg):
-
         if 'bg-blue-500' in msg.target.classes:
-            msg.target.classes= 'w-8 h-48 border-2 border-gray-400 bg-white rounded'
+            msg.target.classes = 'w-8 h-48 border-2 border-gray-400 bg-white rounded'
         else:
-
             msg.target.classes = 'w-8 h-48 border-2 border-gray-400 bg-blue-500 rounded'
 
-    
     def time_dropdown_select(self, msg):
         if msg.target.value == 'weekday':
             self.calendar.resetCalendar()
@@ -52,7 +51,7 @@ class AppointmentPage:
                 day_container = jp.Div(classes='flex flex-col items-center mx-1', a=bars_container)
                 jp.Div(text=day, classes='mb-2', a=day_container)
                 bar = jp.Div(classes='w-8 h-48 border-2 border-gray-400 bg-white rounded', a=day_container)
-                bar.on('click', self.toggle_color) # 막대를 클릭하면 색상 바뀜
+                bar.on('click', self.toggle_color)  # 막대를 클릭하면 색상 바뀜
 
         else:
             self.calendar.resetCalendar()
@@ -71,25 +70,24 @@ class AppointmentPage:
 
     def appointment_page(self):
         wp = jp.WebPage()
-        head = Header("/timepromise/make") 
+        head = Header("/timepromise/make")
         head.show_header(wp)
-    
-        
+
         # 이벤트 이름 입력 필드
         event_name_container = jp.Div(classes='flex justify-center mt-10')
-        event_name_input = jp.Input(placeholder='New Event Name', 
-                                    classes='block w-1/3 p-2 text-center border rounded', 
+        event_name_input = jp.Input(placeholder='New Event Name',
+                                    classes='block w-1/3 p-2 text-center border rounded',
                                     style='border-width: 2px; border-color: black;')
         event_name_container.add(event_name_input)
-        
+
         # 이전 페이지로 이동하는 화살표
         back_arrow = jp.Div(text='<', classes='cursor-pointer absolute top-20 left-5 ' + self.font.Heading2_Bold)
-        
+
         # 달력과 약속 정보를 담을 컨테이너
         main_container = jp.Div(classes='flex justify-between mt-10', style='width: 80%; margin: 0 auto;')
 
         # 달력을 담을 div
-        
+
         title = jp.P(text='시간을 정해주세요', classes='text-xl font-bold mb-2 text-center')
         description = jp.P(text='요일을 선택할 수 있습니다.', classes='text-gray-600 mb-2 text-center')
         description2 = jp.P(text='특정 일을 선택할 수도 있습니다.', classes='text-gray-600 mb-4 text-center')
@@ -104,18 +102,13 @@ class AppointmentPage:
         # 약속 정보를 담을 컨테이너
         right_container = jp.Div(classes='flex flex-col w-1/3 h-96 justify-between')
         # 약속 멘트 정보를 담을 div
-        appointment_ment_div = jp.Div(classes='w-full bg-white-100 p-4 rounded-md', style='margin-bottom: 0;')  
-
+        appointment_ment_div = jp.Div(classes='w-full bg-white-100 p-4 rounded-md', style='margin-bottom: 0;')
 
         title = jp.P(text='몇시에 만날래?', classes='text-xl font-bold mb-2 text-center')
         description = jp.P(text='시작시간 및 끝나는 시간을 선택해 주세요.', classes='text-gray-600 mb-2 text-center')
         description2 = jp.P(text='약속 장소를 선택해 주세요.', classes='text-gray-600 mb-4 text-center')
 
-        selection_dropdown = jp.Select(classes='block w-2/3 mx-auto mb-4', style='width: 200px;')
-        selection_dropdown.add(jp.Option(text='24시간 기준', value='', selected=True, classes='text-gray-500'))  # 선택한 상태로 설정
-        selection_dropdown.add(jp.Option(text='12시간 기준', value='weekday', classes='text-gray-500'))  # 선택 가능하도록 변경
-        appointment_ment_div.add(title, description, description2, selection_dropdown)
-
+        appointment_ment_div.add(title, description, description2)
 
         # 약속 정보를 담을 div
         appointment_info_div = jp.Div(classes='bg-white p-4 shadow-lg rounded-lg mt-1', style='width: 400px; height: 320px;')
@@ -123,86 +116,102 @@ class AppointmentPage:
         # 시작 시간 선택
         start_time_container = jp.Div(classes='flex items-center mb-4')
         start_time_container.add(jp.P(text='시작 시간:', classes='mr-2'))
-        
+
         start_hour_select = jp.Select(classes='block w-1/3')
         start_hour_select.add(jp.Option(text='09시', value='', disabled=True, selected=True, classes='text-gray-500'))
         for hour in range(9, 24):
             start_hour_select.add(jp.Option(text=str(hour).zfill(2), value=str(hour).zfill(2)))
         start_time_container.add(start_hour_select)
-        
+
         start_minute_select = jp.Select(classes='block w-1/3 ml-2')
         start_minute_select.add(jp.Option(text='00분', value='', disabled=True, selected=True, classes='text-gray-500'))
         for minute in range(0, 60, 5):
             start_minute_select.add(jp.Option(text=str(minute).zfill(2), value=str(minute).zfill(2)))
         start_time_container.add(start_minute_select)
-        
+
         # 끝나는 시간 선택
         end_time_container = jp.Div(classes='flex items-center mb-4')
         end_time_container.add(jp.P(text='끝나는 시간:', classes='mr-2'))
-        
+
         end_hour_select = jp.Select(classes='block w-1/3')
         end_hour_select.add(jp.Option(text='18시', value='', disabled=True, selected=True, classes='text-gray-500'))
         for hour in range(9, 25):
             end_hour_select.add(jp.Option(text=str(hour).zfill(2), value=str(hour).zfill(2)))
         end_time_container.add(end_hour_select)
-        
+
         end_minute_select = jp.Select(classes='block w-1/3 ml-2')
         end_minute_select.add(jp.Option(text='00분', value='', disabled=True, selected=True, classes='text-gray-500'))
         for minute in range(0, 60, 5):
             end_minute_select.add(jp.Option(text=str(minute).zfill(2), value=str(minute).zfill(2)))
         end_time_container.add(end_minute_select)
-        
+
         appointment_info_div.add(start_time_container)
         appointment_info_div.add(end_time_container)
-        
+
         # 지역 선택
         appointment_info_div.add(jp.P(text='지역 선택:', classes='mt-4'))
-        self.appointment_place_select = jp.Select(classes='block w-full mt-2 mb-4')
+        self.appointment_place_select = jp.Select(classes='block w-full mt-2 mb-4', change=self.show_custom_place_input)
         self.appointment_place_select.add(jp.Option(text='장소 선택하세요', value='', disabled=True, selected=True, classes='text-gray-500'))
         self.appointment_place_select.add(jp.Option(text='부산대', value='부산대'))
         self.appointment_place_select.add(jp.Option(text='서면', value='서면'))
         self.appointment_place_select.add(jp.Option(text='광안리', value='광안리'))
-        self.appointment_place_select.add(jp.Option(text='남포', value='남포'))        
+        self.appointment_place_select.add(jp.Option(text='남포', value='남포'))
         self.appointment_place_select.add(jp.Option(text='직접 작성', value='직접 작성'))
         appointment_info_div.add(self.appointment_place_select)
-        
+
         # 약속 종류 선택
         appointment_info_div.add(jp.P(text='약속 종류:', classes='mt-4'))
         self.appointment_type_select = jp.Select(classes='block w-full mt-2 mb-4', change=self.show_custom_type_input)
         self.appointment_type_select.add(jp.Option(text='종류를 선택하세요', value='', disabled=True, selected=True, classes='text-gray-500'))
-        self.appointment_type_select.add(jp.Option(text='미정', value='종류는 미정'))
+        self.appointment_type_select.add(jp.Option(text='미정', value='미정'))
         self.appointment_type_select.add(jp.Option(text='밥약속', value='밥약속'))
         self.appointment_type_select.add(jp.Option(text='술약속', value='술약속'))
         self.appointment_type_select.add(jp.Option(text='회의', value='회의'))
         self.appointment_type_select.add(jp.Option(text='직접 작성', value='직접 작성'))
         appointment_info_div.add(self.appointment_type_select)
-        
+
         # 약속 추가 버튼
         appointment_info_div.add(jp.Button(text='약속 추가', classes='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded mt-4'))
 
         # right_container에 약속 멘트 div와 약속 정보 div 추가
         right_container.add(appointment_ment_div, appointment_info_div)
-        
+
         main_container.add(self.calendar_div, right_container)
         wp.add(event_name_container, back_arrow, main_container)
         return wp
-    
-    
+
     # 약속 종류 선택 시 직접 작성 입력란을 보여줌
     def show_custom_type_input(self, msg):
         if msg.value == '직접 작성':
-            # 기존 Select 요소를 숨기고 Input 필드를 추가
             self.appointment_type_select.set_class('hidden')
-            self.custom_type_input = jp.Input(type='text', classes='block w-full mt-2', placeholder='약속 종류를 입력하세요')
-            msg.target.parent.add(self.custom_type_input) # 선택한 요소의 부모 요소에 추가
+            if not self.custom_type_input:
+                self.custom_type_input = jp.Input(type='text', classes='block w-full mt-2', placeholder='약속 종류를 입력하세요')
+                msg.target.add(self.custom_type_input)  # 선택한 요소의 부모 요소에 추가
         else:
-            pass
+            if self.custom_type_input:
+                self.custom_type_input.delete()
+                self.custom_type_input = None
+            self.appointment_type_select.set_class('block')
+
+    # 지역 선택 시 직접 작성 입력란을 보여줌
+    def show_custom_place_input(self, msg):
+        if msg.value == '직접 작성':
+            self.appointment_place_select.set_class('hidden')
+            if not self.custom_place_input:
+                self.custom_place_input = jp.Input(type='text', classes='block w-full mt-2', placeholder='지역을 입력하세요')
+                msg.target.add(self.custom_place_input)  # 선택한 요소의 부모 요소에 추가
+        else:
+            if self.custom_place_input:
+                self.custom_place_input.delete()
+                self.custom_place_input = None
+            self.appointment_place_select.set_class('block')
 
     # 이전 페이지로 돌아가는 함수
-    def go_back(self, msg):     ###### 잘 실행되지 않아 수정필요
+    def go_back(self, msg):
         jp.redirect('/timepromise')
-
 
 def TimePromiseMakeView():
     appointment_page = AppointmentPage()
     return appointment_page.appointment_page()
+
+
